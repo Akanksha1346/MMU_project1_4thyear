39bbaa65743e76f8c025b96f014aa33f0a768484
akanksha <akkisharma1346@gmail.com>
2017-08-09 13:21:56 +0530

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 print("let's get started")
 spy_name=raw_input("provide your name here:")
-
-
 #to check the length of spy name whether it has any input or not
 if len(spy_name)>0:
     spy_age=0
@@ -9,28 +7,26 @@ if len(spy_name)>0:
     spy_is_online=False
     spy_salutation = raw_input("what should we call you?:")
     #concatenation of salutation and name
-
+    spy_age = raw_input("spy age is:")
+    print type(spy_age)
+    spy_age = int(spy_age)
+    print type(spy_age)
+    # age limit on the spy_name
+    if spy_age > 12 and spy_age < 50:
+        spy_rating = float(raw_input("spy rating is:"))
+    if spy_rating >4.5:
+        print "you are great"
+    elif spy_rating >=3.5 and spy_rating <=4.5:
+        print "you are good"
+    elif spy_rating >= 2.5 and spy_rating <= 3.5:
+        print "you are average"
+    elif spy_rating <= 2.5:
+        print "you are below average"
+    else:
+        print "Rigth now! you are not eligible to be spy"
     print "welcome %s %s Glad to have you with us" %(spy_name,spy_salutation)
     print 'alright ' +spy_name+ " " "I'd like to know little bit more about u before we proceed further"
 
 else:
     print"A spy needs to have a valid name,Please try again"
 
-spy_age = raw_input("spy age is:")
-print type(spy_age)
-spy_age = int(spy_age)
-print type(spy_age)
-# age limit on the spy_name
-if spy_age > 12 and spy_age < 50:
-    spy_rating = raw_input("spy rating is:")
-else:
-    print "Rigth now! you are not eligible to be spy"
-#to check how spy will be
-if spy_rating>4.9 and spy_rating<4.5:
-    print "you are great"
-elif spy_rating>3.5  and spy_rating<4.5  :
-    print "you are good"
-elif spy_rating>2.5 and spy_rating<3.5:
-    print "you are average"
-else :
-    print "you are below average"
