adfc4d0ba8c9fc187e4d61e6b5c985429d942224
akanksha <akkisharma1346@gmail.com>
2017-08-13 00:36:08 +0530

    assignments

--- Daily_Assignments/assignment.py
+++ Daily_Assignments/assignment.py
@@ -28,3 +28,49 @@ x=tuple(l)
 print x
 
 5.
+a=raw_input("enter the string")
+words=a.split(',')
+words.sort()
+print ",".join(words)
+
+6.
+a= raw_input("enter the string")
+s=a.upper()
+print s
+
+7.
+
+
+
+8.
+a=raw_input("enter the input:")
+b={"Digits":0 , "Letters":0}
+for c in a:
+    if c.isdigit():
+        b["Digits"]+=1
+    elif c.isalpha():
+        b["Letters"]+=1
+    else:
+        pass
+print "Digits",b["Digits"]
+print "Letters",b["Letters"]
+
+9.
+a=raw_input("enter the input")
+b={"Upper":0,"Lower":0}
+for c in a:
+    if c.islower():
+        b["Lower"]+=1
+    elif c.isupper():
+        b["Upper"]+=1
+    else:
+        pass
+print ("lowercase",b["Lower"])
+print ("uppercase",b["Upper"])
+
+10.
+a=raw_input("enter the no")
+num=x
+for x in a.split(","):
+    if(x%2!=0):
+        print x
---
+++ Daily_Assignments/assignment1.py
@@ -0,0 +1,5 @@
+a = int(raw_input("enter the no"))
+num = a
+for x in a.split(","):
+    if (x % 2 != 0):
+        print x
