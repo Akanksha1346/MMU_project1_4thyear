4df7596108db2524b9d1370329f2e171ed0b2343
akanksha <akkisharma1346@gmail.com>
2017-09-14 20:44:57 +0530

    to read the chats between the spy friend

--- read_message.py
+++ read_message.py
@@ -1,18 +1,35 @@
 from select_friend import select_friend
+from globals import friends
+from termcolor import colored
 from steganography.steganography import Steganography
-from datetime import datetime
-#to read secret messages
+import re
 def read_message():
-  sender = select_friend()
-
-  #to decode secret messages
-  output_path = raw_input("What is the name of the file?")
-  secret_text = Steganography.decode()
-  new_chat = {
-      "message": secret_text,
-      "time": datetime.now(),
-      "sent_by_me": False
-  }
-
-  select_friend[sender]['chats'].append(new_chat)
-  print "Your secret message has been saved!"
+    #function logic
+    friend_choice = select_friend()
+    #check if Friend List is not empty
+    if friend_choice!=(-1):
+        pattern='^[A-Za-z][0-9A-Za-z\s]*\.jpg$'
+        value=True
+        #temporary variable
+        #calculating the Average of Words which is enter
+        friends[friend_choice].calcAverageWords()
+        #create the  message
+        while value:
+                output_image = raw_input("Provide the name of the image to be decrypted: ")
+                if(re.match(pattern,output_image)!=None):
+                    value=False
+                else:
+                    print colored("Enter Again!!!!",'red')
+        try:
+            #Decryption of message
+            text = Steganography.decode(output_image)
+            print "Message:",text
+        #typing error
+        except TypeError:
+            #Blank Image Case i.e. No Decrypted Message in Image
+            print colored("Image does not have any message!!!!",'red')
+        #input or output error
+        except IOError:
+            print colored("Image Does Not Exists!!!!", 'red')
+    else:
+        print colored("Empty Friend's List!!!!",'red')
